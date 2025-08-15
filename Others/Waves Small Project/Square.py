12a9cca34e6472ab708ecd724bd1d8b9221afd6c
JqbPoz <89699784+jqbpoz@users.noreply.github.com>
2025-08-15 22:07:28 +0200

    Square Basic

---
+++ Others/Waves Small Project/Square.py
@@ -0,0 +1,75 @@
+# One ordinary day, someone asked me why the ripples on water remain perfectly circular even after throwing a brick into it.
+# That question inspired the idea of creating a simple visualization of propagating waves according to wave theory.
+#
+# The visualization simulates the two-dimensional wave equation on a discrete grid.
+
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import animation
+from mpl_toolkits.mplot3d import Axes3D
+
+size = 100
+frames = 100
+damping = 0.99
+
+u = np.zeros((size, size))
+u_prev = np.zeros((size, size))
+
+square_size = 10
+start = size // 2 - square_size // 2
+end = start + square_size
+u[start:end, start:end] = 0.5
+
+x = np.arange(0, size)
+y = np.arange(0, size)
+X, Y = np.meshgrid(x, y)
+
+fig = plt.figure(figsize=(12, 6))
+
+ax3d = fig.add_subplot(1, 2, 1, projection='3d')
+surf3d = ax3d.plot_surface(X, Y, u, cmap='viridis', vmin=-1, vmax=1)
+ax3d.set_zlim(-1.2, 1.2)
+ax3d.set_title("Fale 3D")
+ax3d.set_xlabel("X")
+ax3d.set_ylabel("Y")
+ax3d.set_zlabel("Wysokość fali")
+
+ax2d = fig.add_subplot(1, 2, 2)
+img = ax2d.imshow(u, cmap='viridis', vmin=-1, vmax=1, origin='lower')
+ax2d.set_title("Widok z góry")
+ax2d.set_xlabel("X")
+ax2d.set_ylabel("Y")
+fig.colorbar(img, ax=ax2d, shrink=0.8)
+
+def update(frame):
+    global u, u_prev
+    laplacian = (
+            np.roll(u, 1, axis=0) +
+            np.roll(u, -1, axis=0) +
+            np.roll(u, 1, axis=1) +
+            np.roll(u, -1, axis=1) -
+            4 * u
+    )
+    u_new = 2 * u - u_prev + 0.2 * laplacian
+    u_new *= damping
+    u_prev, u = u, u_new
+
+    ax3d.clear()
+    ax3d.set_zlim(-1.2, 1.2)
+    ax3d.set_title("Fale 3D")
+    ax3d.set_xlabel("X")
+    ax3d.set_ylabel("Y")
+    ax3d.set_zlabel("Wysokość fali")
+    ax3d.plot_surface(X, Y, u, cmap='viridis', vmin=-1, vmax=1)
+
+    ax2d.clear()
+    ax2d.set_title("Widok z góry")
+    ax2d.set_xlabel("X")
+    ax2d.set_ylabel("Y")
+    ax2d.imshow(u, cmap='viridis', vmin=-1, vmax=1, origin='lower')
+
+    return []
+
+
+ani = animation.FuncAnimation(fig, update, frames=frames, interval=50, blit=False)
+plt.show()
